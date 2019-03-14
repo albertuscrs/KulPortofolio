c905bf8c85ad4aff83df9046550c8f54414cba21
albertuscrs <achristyadhi@gmail.com>
2019-03-14 18:44:43 +0700

    Added annotated model for blog, portfolio, and skill

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  slug       :string
+#  status     :integer          default("draft")
+#
+
 class Blog < ApplicationRecord
     enum status: { draft: 0, published: 1 }
     extend FriendlyId

aeef578afcc7c89627614ae042f32e762729d35b
albertuscrs <achristyadhi@gmail.com>
2019-03-15 15:09:20 +0700

    Implemented concer for managing data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -10,5 +10,14 @@
 #
 
 class Skill < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :percent_utilized
+
+    #after def new in controller automaticly run
+    after_initialize :set_defaults
+
+    #setting the defaults
+    def set_defaults
+        self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+    end
 end

fd145cd83128ae185a61a99f0c3407fef9ad8dd4
albertuscrs <achristyadhi@gmail.com>
2019-03-18 15:23:34 +0700

    Added guest user for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    include DeviseWhitelist, SetSource
+    include DeviseWhitelist,
+            SetSource,
+            CurrentUserConcern
     #include SetSource
     
+    
+
 end

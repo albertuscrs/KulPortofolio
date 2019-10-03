0e10e12faadfd01ad617ad681bda80c36d077b54
albertuscrs <achristyadhi@gmail.com>
2019-10-03 15:59:26 +0700

    126 - Updated master layout styles

--- Gemfile
+++ Gemfile
@@ -54,7 +54,7 @@ end
 
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'bootstrap', '~> 4.0.0'
+gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'devise', '~> 4.7', '>= 4.7.1'
 gem 'friendly_id', '~> 5.2.4'
 gem 'kul_view_tool', '~> 0.1.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
+# Helper
 module ApplicationHelper
-
-    def login_helper
-        if current_user.is_a?(GuestUser) 
-            (link_to "Login", new_user_session_path) + "<br>".html_safe + (link_to "Sign Up", new_user_registration_path)
-        else 
-            link_to "Logout", destroy_user_session_path, method: :delete
-        end 
+  def login_helper style
+    if current_user.is_a?(GuestUser)
+      (link_to "Login", new_user_session_path, class: style) + " ".html_safe + (link_to "Sign Up", new_user_registration_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
+  end
 
-    def source_helper(layout_name)
-        if session[:source] 
-            greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
-            content_tag(:p, greeting, class: "source-greeting")
-        end
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
+      content_tag(:p, greeting, class: "source-greeting")
     end
+  end
 
-    def copyright_generator
-        KulViewTool::Renderer.copyright 'Albertus Christyadhi', 'All rights reserved'
-    end
+  def copyright_generator
+    KulViewTool::Renderer.copyright 'Albertus Christyadhi', 'All rights reserved'
+  end
 end
